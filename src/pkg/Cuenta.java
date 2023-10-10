fd579feaa15808a8e4c06e9ba40ee32217d28fd7
usuario <usuario@pc20-024>
2023-10-10 10:58:30 +0200

    TDD, paso 1(el test Retirar ha dado error debido a la falta de implementacion)

---
+++ src/pkg/Cuenta.java
@@ -0,0 +1,25 @@
+package pkg;
+
+public class Cuenta {
+	double saldo;
+
+	public void ingresar(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void Retirar(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public double getSaldo() {
+		return 1000;
+	}
+
+	public Cuenta(String NumIdetificacion,String titular,double saldo) {
+		super();
+		this.saldo = saldo;
+	}
+
+}
---
+++ src/test/CuentaTest.java
@@ -0,0 +1,43 @@
+package test;
+import pkg.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CuentaTest {
+
+	static Cuenta cu;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		cu = new Cuenta("12345","Juan",0);
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testIngresar() {
+		cu.ingresar(1000);
+		assertEquals(1000,cu.getSaldo());
+	}
+	@Test
+	void testRetirar() {
+	
+	}
+
+}
